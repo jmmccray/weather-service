bde4d48324215cf12554baf50d2827fab64c2b37
James McCray <jmccray2014@gmail.com>
2024-03-25 22:17:21 -0700

    added viper config manager to protect api keys

---
+++ config/server_config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"fmt"
+	"os"
+)
+
+func LoadServerConfig() error {
+	fmt.Println("Starting to load config...")
+
+	// Check if config.env path exists in the root directory.
+	if _, err := os.Stat("config.env"); os.IsNotExist(err) {
+		fmt.Printf("File '%s' does not exist\n", "env.config")
+		return err
+	} else {
+		viper.SetConfigFile("config.env")
+	}
+
+	// Check if local config.env file can be read-in.
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Unable to read in config.env")	
+		return err
+	}
+
+	// Check if OpenWeather API key is set.
+	variable := viper.GetString( "OW_API_KEY")	
+	if variable == "" {
+		fmt.Println("The config variable, OW_API_KEY does not exist")
+		return err
+	}
+
+	fmt.Println("...Finished successfully loading config")
+	return nil
+}
+
+func ConfigGetString(variable string) string {
+	return viper.GetString(variable)
+}
