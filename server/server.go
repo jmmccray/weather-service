76aeeef3853a954efd1e807308cf093394439b80
James McCray <jmccray2014@gmail.com>
2024-03-29 16:21:57 -0700

    added sel option prompt

--- server/server.go
+++ server/server.go
@@ -100,17 +100,22 @@ type SysData struct {
 }
 
 // TODO: Add OpenWeatherStatusCodes.
-var OpenWeatherStatusCode map[int]string = map[int]string{}
+var openWeatherStatusCode map[int]string = map[int]string{}
 
 // TODO: Elaborate on the Geolocation structs.
-var Geolocations []Geolocation = []Geolocation{
+var geolocations []Geolocation = []Geolocation{
 	{City: "San Diego", State: "CA", Latitude: 32.715736, Longitude: -117.161087},
 	{City: "San Francisco", State: "CA", Latitude: 37.828724, Longitude: -122.355537},
-	{City: "Houston", State: "TX", Latitude: 29.749907, Longitude: -95.358421}}
+	{City: "Houston", State: "TX", Latitude: 29.749907, Longitude: -95.358421},
+}
 
 // Create an OpenWeather client
 func (c *OpenWeatherClient) NewClient() {
 	c.client = &http.Client{}
+}
+
+func (s *OpenWeatherServer) NewServer() {
+	s.server = &http.Server{}
 }
 
 // Make an HTTP GET request from OpenWeather client
@@ -146,12 +151,8 @@ func (c *OpenWeatherClient) ClientWeatherRequest(lat, lon string) error {
 		time.Sleep(2 * time.Second)
 	}
 	defer resp.Body.Close()
-
+	fmt.Println()
 	return nil
-}
-
-func (s *OpenWeatherServer) NewServer() {
-	s.server = &http.Server{}
 }
 
 func (s *OpenWeatherServer) RunServer() {
@@ -163,7 +164,6 @@ func (s *OpenWeatherServer) RunServer() {
 	}
 
 	//http.HandleFunc("/client",getWeatherDataHandler)
-
 	s.server = &http.Server{
 		Addr:         "localhost:8080", // added localhost to by-pass Windows Defender firewall
 		Handler:      http.HandlerFunc(getWeatherDataHandler),
@@ -171,20 +171,16 @@ func (s *OpenWeatherServer) RunServer() {
 		WriteTimeout: 10 * time.Second,
 	}
 
-	fmt.Printf("Running on %s\n", s.server.Addr)
+	fmt.Printf("OpenWeather server is running on %s\n", s.server.Addr)
 
 	if err := s.server.ListenAndServe(); err != nil {
 		panic(err)
 	}
-}
-
-// Converts float geolocation coordinates into a string format.
-func convertGeoCoors(coor float64) string {
-	return fmt.Sprintf("%.6f", coor)
+	
 }
 
 // Spins up a standalone the Weather Server instance on localhost:8080
-func RunOpenWeatherServer() {
+func RunOpenWeatherServer(domain, port string) {
 	// Load necessary environment variables to get OpenWeather data
 	err := config.LoadServerConfig()
 	if err != nil {
@@ -193,13 +189,13 @@ func RunOpenWeatherServer() {
 	}
 
 	server := &http.Server{
-		Addr:         "localhost:8080", // added localhost to by-pass Windows Defender firewall
+		Addr:         fmt.Sprintf("%s:%s",domain,port), // added localhost to by-pass Windows Defender firewall
 		Handler:      http.HandlerFunc(getWeatherDataHandler),
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 
-	fmt.Printf("Running on %s\n", server.Addr)
+	fmt.Printf("Running OpenWeather server on %s\n", server.Addr)
 
 	if err := server.ListenAndServe(); err != nil {
 		panic(err)
@@ -214,15 +210,22 @@ func getWeatherDataHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		fmt.Printf("Error reading response body: %v\n", err)
+		w.Write([]byte(fmt.Sprintf("Error reading response body: %v\n", err)))
+		return
+	}
+
+	if len(body) == 0 {
+		fmt.Printf("Error: Request body is empty")
+		w.Write([]byte(`Error: Request body is empty`))
 		return
 	}
 
 	// Unmarshal data into OpenWeatherData object
 	err = json.Unmarshal(body, &coordinates)
 	if err != nil {
-		fmt.Printf("Error unmarshaling data into Coordinates object: %s\n", err.Error())
-		coordinates.Latitude = "0"
-		coordinates.Longitude = "0"
+		fmt.Printf("Error unmarshaling data into Coordinates struct: %s\n", err.Error())
+		w.Write([]byte(fmt.Sprintf("Error unmarshaling data into Coordinates struct: %s\n", err.Error())))
+		return
 	}
 
 	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s",
@@ -233,14 +236,16 @@ func getWeatherDataHandler(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get(url)
 
 	if err != nil {
-		fmt.Printf("Error sending request: %v\n", err.Error())
+		fmt.Printf("Error sending request: %s\n", err.Error())
+		w.Write([]byte(fmt.Sprintf("Error sending request: %s\n",err.Error())))
 		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
 		fmt.Println("Status Code: ", resp.StatusCode)
-		fmt.Println("Invalid Coordinates: Lat=%d or Lon=%d", 0, 0)
+		fmt.Printf("Invalid Coordinates: Lat=%s or Lon=%s\n", coordinates.Latitude, coordinates.Longitude)
+		w.Write([]byte(fmt.Sprintf("Status Code: %d",resp.StatusCode)))
 		return
 	}
 
@@ -250,15 +255,15 @@ func getWeatherDataHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte(prettyFormatWeatherData([]byte(body))))
-
-	fmt.Println("Weather Data: \n", prettyFormatWeatherData([]byte(body)))
+	w.Write([]byte(prettyFormatWeatherData(body)))
+	fmt.Println("Weather Data: \n", prettyFormatWeatherData(body))
 
 	// Unmarshal data into OpenWeatherData object
 	err = json.Unmarshal(body, &weatherData)
 
 	if err != nil {
 		fmt.Printf("Error unmarshaling data into OpenWeatherData object: %s\n", err.Error())
+		return
 	}
 	// TODO: write struct to SQL DB
 }
@@ -288,7 +293,13 @@ func RunOpenWeatherClient() {
 		time.Sleep(2 * time.Second)
 		i++
 	}
+	fmt.Println()
 	defer resp.Body.Close()
+}
+
+// Converts float geolocation coordinates into a string format.
+func convertGeoCoors(coor float64) string {
+	return fmt.Sprintf("%.6f", coor)
 }
 
 func validateCheckLatLonValue(lat, lon float64) error {
@@ -355,10 +366,33 @@ func getCLIPrompt() {
 			err = client.ClientWeatherRequest(convertGeoCoors(lat), convertGeoCoors(lon))
 			if err != nil {
 				fmt.Println("Error: ", err.Error())
-			}		
-		// TODO: implement select
+			}
+			// TODO: implement select
 		} else if userInput == "sel" {
-			fmt.Println("Select from list")
+			fmt.Println("Select from Menus:")
+
+			// Generate selection menu
+			menu := ""
+			for i, loc := range geolocations {
+				menu += fmt.Sprintf("%d. %s, %s\n",i, loc.City,loc.State)
+			}
+			fmt.Println(menu)
+			
+			fmt.Print("Enter #: ")
+			scanner.Scan()
+
+			// Gets user input, removes all of its whitespace and lowercases each character.
+			userInput := scanner.Text()
+
+			index, err := strconv.Atoi(userInput)
+			if err != nil {
+				fmt.Println("The input is not a valid number: ", index)
+			}
+
+			if index < 0 && index > len(geolocations)-1 {
+				fmt.Printf("The number entered does exist in range [0-%d]\n",len(geolocations)-1)
+			}
+			client.ClientWeatherRequest(convertGeoCoors(geolocations[index].Latitude), convertGeoCoors(geolocations[index].Longitude))
 		} else {
 			fmt.Printf("Invalid input: %s\n", userInput)
 		}
@@ -375,7 +409,7 @@ func StartOpenWeatherApp() {
 	server := &OpenWeatherServer{}
 	go server.RunServer()
 	time.Sleep(3 * time.Second)
-	
+
 	// Allows the user to input an incorrect response 5x.
 	for i := 1; i <= 5; {
 		// Check if the user wants to use the cli or web app interfaces.
@@ -405,9 +439,15 @@ func prettyFormatWeatherData(data []byte) string {
 	var prettyJSON bytes.Buffer
 	err := json.Indent(&prettyJSON, data, "", " ")
 	if err != nil {
-		fmt.Println("Error identing JSON payload", err.Error())
+		fmt.Println("Error indenting JSON payload", err.Error())
 	}
 	return prettyJSON.String()
+
+	// newdata, err := json.MarshalIndent(data, "", " ")
+	// if err != nil {
+	// 	fmt.Println("Error indenting JSON payload", err.Error())
+	// }
+	// return string(newdata)
 }
 
 // TODO: Prints the values from the OpenWeatherData struct
